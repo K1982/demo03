3f5f4a1c689cae65f0f24356501cb865e977e7d1
Tiia Itkonen <K1982@student.jamk.fi>
2016-01-27 11:04:14 +0200

    Initial commit include car&T1

---
+++ T1/Heater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Heater
+    {
+        //properties
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+        public bool IsOn { get; set; }
+
+
+
+
+        // print object data to screen
+        public void PrintData()
+        {
+            Console.WriteLine("Heater data: ");
+            Console.WriteLine("- Temperature : {0}", Temperature);
+            Console.WriteLine("- Humidity : {0}", Humidity);
+            Console.WriteLine("- IsOn : {0}", IsOn);
+        }
+
+        //method returns object properties in a one string
+        public override string ToString()
+        {
+            return "Temperature = " + Temperature + " Humidity = " + Humidity + " IsOn = " + IsOn;
+        }
+
+    }
+}
---
+++ T1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //add new heater
+            Heater heater = new Heater();
+            heater.PrintData();
+            heater.Temperature=50;
+            heater.Humidity = 20;
+            heater.IsOn = true;
+            heater.PrintData();
+            Console.WriteLine(heater.ToString());
+
+            Console.WriteLine("Temperature = " + heater.Temperature);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
